116ef3b62233bb83612c4c32e65cd7326d8e01a9
Harsh Devmurari <harshdevmurari007@gmail.com>
2021-12-24 20:50:16 +0530

    Completed project

---
+++ app.py
@@ -0,0 +1,285 @@
+import os
+import sqlite3
+import cv2 as cv
+import numpy as nu
+# import pyzbar.pyzbar as pyzbar (Scanning QR Code)
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+import flask_session
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, user_tracked_location, input_location_coords, password_check
+
+
+# Configure application
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+
+# Connecting to database
+conn = sqlite3.connect("database.db", timeout = 50, check_same_thread=False)
+db = conn.cursor()
+try:
+    sqliteConnection = sqlite3.connect("database.db")
+    print("Database created and succcessfully connected to SQLite")
+
+    sqlite_select_Query = "select sqlite_version();"
+    db.execute(sqlite_select_Query)
+    record = db.fetchall()
+    print("SQlite database version is: ", record)
+except:
+    print(f"Error while connecting to sqlite")
+
+
+
+# Database one time generating table
+'''
+db.execute("CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT, email TEXT, password TEXT)")
+
+db.execute(""" /*hjhj*/
+CREATE TABLE colleges(
+SrNo DOUBLE,
+Merit_Score VARCHAR(100),
+Choice_Code DOUBLE,
+Institute VARCHAR(100),
+Course_Name VARCHAR(100),
+Exam_JEEMHT__CET VARCHAR(100),
+Type VARCHAR(100),
+Seat_Type VARCHAR(100)
+);""")
+'''
+
+
+
+# Loading colleges info in SQLite table
+"""
+with open('cetcell 2020.csv','r') as fin:
+    dr = csv.DictReader(fin)
+    to_db = [(i['Sr.No.'], i['Merit (Score)'], i['Choice Code'], i['Institute'], i['Course Name'], i['Exam (JEE/MHT  CET)'], i['Type'], i['Seat Type']) for i in dr]
+db.executemany("INSERT INTO colleges (SrNo, Merit_Score, Choice_Code, Institute, Course_Name, Exam_JEEMHT__CET, Type, Seat_Type) VALUES (?, ?, ?, ?, ?, ?, ?, ?);", to_db)
+conn.commit()
+print("clg insertion successfull")
+"""
+
+
+
+# APP ROUTES
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    # If user reached route via POST
+    if request.method == "POST":
+        # Getting info
+        name = request.form.get("name")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Check whether password is strong enough
+        x = password_check(password)
+        if x is False:
+            return apology("Please satisfy password requirements")
+
+        # Checking if password equal to confirmation
+        if not password == confirmation:
+            return apology("Password and confirmation didnt matched")
+
+        # Generating hash to store in database
+        hash = generate_password_hash(password)
+
+        # Storing in database
+        db.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hash])
+        # print("tables interior: ")
+        db.execute("SELECT * FROM users")
+        # print(f"fetch of register", db.fetchall())
+
+        return redirect("/login")
+    
+    # If user reached route via GET
+    else:
+        return render_template("register.html")
+
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    # If user reached route via POST
+    if request.method == "POST":
+        # Forget any previous user
+
+        # Getting info
+        name = request.form.get("name")
+        password = request.form.get("password")
+
+        # Validating info with database
+        db.execute("SELECT * FROM users")
+        rows = db.fetchall()
+        # print(f"fetch of login: {rows}")
+        for row in rows:
+            if row[1] == name:
+                # Remember which user has logged in
+                #session["id"] = rows[0][0]
+                print("successfull login")
+                return redirect("/details")
+        return apology("Please register if you havent")
+
+    # If user reached route via GET
+    else:
+        return render_template("login.html")
+
+
+
+@app.route("/details", methods=["GET", "POST"])
+def details():
+    # If user reached route via POST
+    if request.method == "POST":
+        # Getting user info
+        cet = request.form.get("cet") # Getting input via scanning QR of marklist
+        jee = request.form.get("jee")
+        location = request.form.get("location") # Location tracking using ip address
+        entered_location = request.form.get("entered_location")
+        ce = request.form.get("Computer Engineering")
+        it = request.form.get("Information Technology")
+        cse = request.form.get("Computer Science and Engineering(Data Science)")
+        ai = request.form.get("Artificial Intelligence and Data Science")
+        mech = request.form.get("Mechanical Engineering")
+        ee = request.form.get("Electronics Engineering")
+        che = request.form.get("Chemical Engineering")
+        et = request.form.get("Electronics and Telecommunication\nEngg")
+        ae = request.form.get("Automobile Engineering")
+
+        # Basic errorhandeling
+        if cet=="" and jee=="":
+            return apology("Atleast one marks are required")
+        
+        if location == "enter":
+            # Find and use API
+            print(f"Users entered location is: {entered_location}")
+        elif location == "track":
+            userlocation = user_tracked_location()
+            # print(f"Your current location is: {userlocation}")
+        else:
+            return apology("please provide location") 
+
+        # Scanning QR code
+        '''
+        NOT IMPLEMENTED DUE TO PYZBAR ".dll" DISCREPANCY
+        cap = cv.VideoCapture(2)
+
+        while True:
+            _, frame = cap.read()
+
+            barcode = pyzbar.decode(frame)
+            for bdata in barcode:
+                print(f"data  {bdata.data}")
+                Data = barcode.data.decode("utf-8")
+                Type = barcode.type
+                print(f"data  {Data}")
+                print(f"type  {Type}")
+            break
+        '''
+
+        # Database query for best clg suggestions
+        li = [ce, it, cse, ai, mech, ee, che, et, ae]
+        fli = []
+        ffli = []
+        for item in li:
+            if item != None:
+                fli.append([item])
+
+        for item in range(len(fli)):
+            ffli.append(fli[item][0])
+
+        params = []
+        params.append(jee)
+        for item in ffli:
+            params.append(item)
+        # print(f"params: {params}")
+        
+        # Database query for colleges in which admission is possible due to jee
+        db.execute("SELECT * FROM colleges WHERE Merit_Score<=? AND Exam_JEEMHT__CET='JEE' AND Course_Name IN (%s)"%', '.join('?' for a in ffli), params)
+        global clg_jee_list
+        clg_jee_list = db.fetchall()
+        # print(f"clg_jee_list: {clg_jee_list}")
+
+        params[0] = cet
+        #print(f"params: {params}")
+        # Database query for colleges in which admission is possible due to mht-cet
+        db.execute("SELECT * FROM colleges WHERE Merit_Score<=? AND Exam_JEEMHT__CET='MHT CET' AND Course_Name IN (%s)"%', '.join('?' for a in ffli), params)
+        global clg_cet_list
+        clg_cet_list = db.fetchall()
+        # print(f"clg_cet_list: {clg_cet_list}")
+
+        # Merging both jee and mht-cet lists
+        global clg_list 
+        clg_list = clg_jee_list + clg_cet_list
+        # print(f"clg_list: {clg_list}")
+
+        '''
+        APLTERNATE CORRECT APPROCH BUT NOT WORKING PROPERLY DUE TO DISCREPANCY IN DATA
+        skips some list items and displays it...........
+
+        for list in clg_list:
+            if (list[5] == "JEE" and list[1] > jee) or (list[5] == "MHT CET" and list[1] > cet):
+                print(list)
+                print()
+                clg_list.remove(list)
+
+        print(f"clg_list : {clg_list}")
+        '''
+
+        return redirect("/suggestions")
+    # If user reached route via GET
+    else:
+        return render_template("details.html")
+
+
+
+@app.route("/suggestions")
+def suggestions():
+    # Rendering suggestions along with clg list generated in runtime
+    return render_template("suggestions.html", clg_list = clg_list)
+
+
+
+@app.route("/faqs")
+def faqs():
+    return render_template("faqs.html")
+
+
+
+# Error handlers
+def errorhandler(e):
+    # Basic error handeling
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
+
+
+
+app.run(debug = True)
+conn.close()
---
+++ helpers.py
@@ -0,0 +1,96 @@
+import os
+import requests
+import json
+import pandas as pd
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+
+# Apology renders memetemplate as an userside error
+def apology(message, code=400):
+    # Render message as an error to user.
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
+
+# Function used above every other function which ensures user is logged in
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+
+# Checks password requirements
+def password_check(ps):
+    # Ensures password contains atleast one uppercase, one lowercase and one numeric.
+    uc = False
+    lc = False
+    nm = False
+    print(type(uc))
+    for char in ps:
+        if char.isupper():
+            uc = True
+        if char.islower():
+            lc = True
+        if char.isdigit():
+            nm = True
+
+    if uc and lc and nm:
+        return True
+    else:
+        return False
+
+
+
+# Tracks users location via geolocation api
+def user_tracked_location():
+    # Returns bunch of user location info along with latitude and longitude
+    request_url = 'https://geolocation-db.com/jsonp/'
+    response = requests.get(request_url)
+    result = response.content.decode()
+    result = result.split("(")[1].strip(")")
+    result  = json.loads(result)
+    print(result)
+    lat = result["latitude"]
+    lon = result["longitude"]
+    location = [lat, lon]
+    return location
+
+
+
+# Geolocation api for location name to coordinates(latitude and longitude)
+def input_location_coords(l):
+    """
+    # COULDNT FIND GEOCOORDINATE'S FREE API
+
+    1) POSITIONSTACK.COM (not accurate)
+    api_key = "f5e4086ff8d0e514ad9ab4d30b203ffe"
+    base_url = "http://api.positionstack.com/v1/forward?access_key=f5e4086ff8d0e514ad9ab4d30b203ffe&query=nallsopara"
+    response = requests.get(base_url).json()
+    print(response)
+
+    2) GOOGLE API
+    api_key = "AIzaSyCp2GycvRmkfGlcaK4eJiCsOE1x_NOoVRs" (requires bank details)
+    base_url = "https://maps.googleapis.com/maps/api/geocode/json?address=nallasopara&key=AIzaSyCp2GycvRmkfGlcaK4eJiCsOE1x_NOoVRs"
+    response = requests.get(base_url).json()
+    """
